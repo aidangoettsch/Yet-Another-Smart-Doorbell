d5e31862d85d77316e675c1b922c5e5b7ef0684c
MagicalTaterTot <shadowbox826@gmail.com>
2018-03-11 00:39:30 -0500

    testing fixes

--- compare.py
+++ compare.py
@@ -1,5 +1,16 @@
 import face_recognition as fr
-import os
+import os, glob
 
-fileCount = next(os.walk(dir))[2]
-fileCount = len(fileCount)
+path, dirs, files = os.walk("/pictures/base").next()
+fileCount = len(files)
+
+baseImages = []
+baseNames = []
+
+os.chdir("/pictures/base")
+for file in glob.glob("*.png"):
+    baseNames.append(file)
+
+i = 0
+while i < fileCount:
+    baseImages.append(fr.load_image_file(baseNames[i]))
