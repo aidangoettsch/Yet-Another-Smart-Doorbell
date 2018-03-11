11b48a7834db2e8f575adfec1af23e47490794c1
MagicalTaterTot <shadowbox826@gmail.com>
2018-03-11 03:15:15 -0400

    hopefully done with compare

--- facial-recognition/compare.py
+++ facial-recognition/compare.py
@@ -11,17 +11,21 @@ imageArray = []
 baseImages = []
 baseFilenames = []
 baseNames = []
+resultsArray = []
+encodedArray = []
 
 os.chdir("pictures/base")
 for f in glob.glob("*.png"):
     baseFilenames.append(f)
 
-
 i = 0
 while i < fileCount:
 	baseImages.append(fr.load_image_file(baseFilenames[i]))
 	i += 1
-"""
+
+for img in baseImages:
+    encodedArray.append(fr.face_encodings(img)[0])
+
 #Webserver
 hostName = ""
 hostPort = 8122
@@ -30,7 +34,7 @@ class MyServer(BaseHTTPRequestHandler):
 	def do_POST(self):
 		global imageArray
 		print( "Incoming HTTP: ", self.path )
-        
+
 		contentLength = int(self.headers['Contect Length'])
 		postData = self.rfile.read(contentLength)
 		img = base64.decodebytes(postData)
@@ -50,8 +54,16 @@ except KeyboardInterrupt:
 
 myServer.server_close()
 print(time.asctime(), "Server Stops - %s:%s" % (hostName, hostPort))
-"""
+
 #Comparison
 for fname in baseFilenames:
     baseNames.append(fname[0:(len(fname)-4)])
 print(baseNames)
+
+unknownEncode = fr.face_encodings(fr.load_image_file(imageArray))
+
+j = 0
+while j < len(baseImages):
+    result = fr.compare_faces([encodedArray[i]], unknownEncode)
+    resultsArray.append(result)
+    j += 1
